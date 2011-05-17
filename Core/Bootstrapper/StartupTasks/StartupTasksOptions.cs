24b5ff04cef0e07c10982d4bb1ac3f86bf18c062
Luis Bocaletti <luis.bocaletti@gmail.com>
2011-05-17 08:08:18 -0500

    Added StartupTasksOptions.cs (Broken tests)

---
+++ Core/Bootstrapper/StartupTasks/StartupTasksOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Bootstrap.Extensions;
+
+namespace Bootstrap.StartupTasks
+{
+    public class StartupTasksOptions: BootstrapperExtensionOptions
+    {
+        public List<Type> Sequence { get; private set; }
+
+        public StartupTasksOptions UsingSequence(ISequenceSpecification sequenceSpecification)
+        {
+            Sequence = sequenceSpecification.Sequence;
+            return this;
+        }
+
+    }
+}
