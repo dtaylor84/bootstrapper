bf5a1a0ea7e0c4a976e78cc1369136541ceabf81
Luis Bocaletti <luis.bocaletti@gmail.com>
2011-05-17 08:08:18 -0500

    Added StartupTasksOptions.cs (Broken tests)

---
+++ Tests/Bootstrapper.Tests/Core/StartupTasks/StartupTasksOptionsTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Bootstrap.StartupTasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bootstrap.Tests.Core.StartupTasks
+{
+    [TestClass]
+    public class StartupTasksOptionsTests
+    {
+        [TestMethod]
+        public void ShouldCreateANewStartupTasksOptions()
+        {
+            //Act
+            var result = new StartupTasksOptions();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(StartupTasksOptions));
+        }
+
+        [TestMethod]
+        public void ShouldReturnAnEmptySequence()
+        {
+            //Act
+            var options = new StartupTasksOptions();
+            var result = options.Sequence;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(List<Type>));
+            options.UsingSequence(new SequenceSpecification().First<TaskAlpha>().Then<TaskBeta>());
+        }
+
+        [TestMethod]
+        public void ShouldReturnASequenceSpecification()
+        {
+            //Act
+            var options = new StartupTasksOptions();
+            var result = options.Sequence;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(List<Type>));
+        }
+
+
+    }
+}
