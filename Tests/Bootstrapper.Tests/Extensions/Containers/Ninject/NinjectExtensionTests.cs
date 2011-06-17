fd1011e4eaf05b956365d03b7778daa1fc1313e2
luisbocaletti <luisbocaletti@localhost>
2011-06-17 15:39:53 -0500

    Added Bootstrap.Ninject

---
+++ Tests/Bootstrapper.Tests/Extensions/Containers/Ninject/NinjectExtensionTests.cs
@@ -0,0 +1,415 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bootstrap.Extensions.Containers;
+using Bootstrap.Extensions.StartupTasks;
+using Bootstrap.Ninject;
+using Bootstrap.Tests.Core.Extensions.StartupTasks;
+using Bootstrap.Tests.Extensions.TestImplementations;
+using Bootstrap.Tests.Other;
+using FakeItEasy;
+using Microsoft.Practices.ServiceLocation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+
+namespace Bootstrap.Tests.Extensions.Containers.Ninject
+{
+    [TestClass]
+    public class NinjectExtensionTests
+    {
+        [TestInitialize]
+        public void InitializeBootstrapper()
+        {
+            Bootstrapper.ClearExtensions();
+        }
+
+        [TestMethod]
+        public void ShouldCreateANinjectExtension()
+        {
+            //Act
+            var result = new NinjectExtension();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(NinjectExtension));
+        }
+
+        [TestMethod]
+        public void ShouldAddNinjectToExcludedAssemblies()
+        {
+            //Act
+            var result = new NinjectExtension();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(Bootstrapper.Excluding.Assemblies.Contains("Ninject"));
+        }
+
+        [TestMethod]
+        public void ShouldReturnANullContainer()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            var result = containerExtension.Container;
+            
+            //Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void ShouldReturnAnIKernel()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            containerExtension.Run();
+            var result = containerExtension.Container;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IKernel));
+        }
+
+        [TestMethod]
+        public void ShouldRegisterAllTypesOfIBootstrapperRegistration()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            containerExtension.Run();
+            var result = containerExtension.ResolveAll<IBootstrapperRegistration>();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IEnumerable<IBootstrapperRegistration>));
+            Assert.IsTrue(result.Count > 0);
+        }
+
+        [TestMethod]
+        public void ShouldRegisterAllTypesOfINinjectRegistration()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            containerExtension.Run();
+            var result = containerExtension.ResolveAll<INinjectRegistration>();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IEnumerable<INinjectRegistration>));
+            Assert.IsTrue(result.Count > 0);
+        }
+
+        [TestMethod]
+        public void ShouldInvokeTheRegisterMethodOfAllIBootstrapperRegistrationTypes()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            containerExtension.Run();
+            var result = containerExtension.ResolveAll<IStartupTask>();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IList<IStartupTask>));
+            Assert.IsTrue(result.Count > 0);
+        }
+
+        [TestMethod]
+        public void ShouldInvokeTheRegisterMethodOfAllIStructureMapRegistrationTypes()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            containerExtension.Run();
+            var result = containerExtension.Resolve<NinjectExtension>();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(NinjectExtension));
+        }
+
+        [Ignore] //TODO: Fix by adding NinjectServiceLocator
+        [TestMethod]
+        public void ShouldSetTheServiceLocator()
+        {
+            //Arrange
+            ServiceLocator.SetLocatorProvider(() => null);
+            var containerExtension = new NinjectExtension();
+            containerExtension.Run();
+
+            //Act            
+            containerExtension.SetServiceLocator();
+            var result = ServiceLocator.Current;
+
+            //Assert
+            Assert.IsNotNull(result);
+            //Assert.IsInstanceOfType(result, typeof(NinjectServiceLocator));
+        }
+
+        [TestMethod]
+        public void ShouldResetTheServiceLocator()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+            containerExtension.Run();
+
+            //Act
+            containerExtension.ResetServiceLocator();
+
+            //Assert
+            Assert.IsNull(ServiceLocator.Current);
+        }
+
+        [TestMethod]
+        public void ShouldSetTheContainer()
+        {
+            //Arrange            
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            containerExtension.Run();
+            var result = containerExtension.Container;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IKernel));
+        }
+
+        [TestMethod]
+        public void ShouldResetTheContainer()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+            containerExtension.Run();
+
+            //Act
+            containerExtension.Reset();
+
+            //Assert
+            Assert.IsNull(containerExtension.Container);
+        }
+
+        [TestMethod]
+        public void ShouldInitializeTheContainerToTheValuePassed()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+            var container = A.Fake<IKernel>();
+
+            //Act
+            containerExtension.InitializeContainer(container);
+            var result = containerExtension.Container;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IKernel));
+            Assert.AreSame(result, container);
+        }
+
+        [TestMethod]
+        public void ShouldResolveASingleType()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+            var container = new StandardKernel();
+            var instance = new object();
+            container.Bind<object>().ToConstant(instance);
+            containerExtension.InitializeContainer(container);
+
+            //Act
+            var result = containerExtension.Resolve<object>();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreSame(instance, result);
+        }
+
+        [TestMethod]
+        public void ShouldResolveMultipleTypes()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+            var container = new StandardKernel();
+            container.Bind<IStartupTask>().To<TaskAlpha>();
+            container.Bind<IStartupTask>().To<TaskBeta>();
+            containerExtension.InitializeContainer(container);
+
+            //Act
+            var result = containerExtension.ResolveAll<IStartupTask>();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Any(t => t is TaskAlpha));
+            Assert.IsTrue(result.Any(t => t is TaskBeta));
+        }
+
+        [TestMethod]
+        public void ShouldRegisterWithTargetAndImplementationType()
+        {
+            //Arrange
+            var container = new StandardKernel();
+            var containerExtension = new NinjectExtension();
+            containerExtension.InitializeContainer(container);
+
+            //Act
+            containerExtension.Register<IBootstrapperContainerExtension, NinjectExtension>();
+            var result = container.Get<IBootstrapperContainerExtension>();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(NinjectExtension));
+        }
+
+        [TestMethod]
+        public void ShouldRegisterWithTargetAndImplementationInstance()
+        {
+            //Arrange
+            var container = new StandardKernel();
+            var containerExtension = new NinjectExtension();
+            containerExtension.InitializeContainer(container);
+
+            //Act
+            containerExtension.Register<IBootstrapperContainerExtension>(containerExtension);
+            var result = container.Get<IBootstrapperContainerExtension>();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(NinjectExtension));
+            Assert.AreSame(containerExtension, result);
+        }
+
+        [TestMethod]
+        public void ShouldRegisterWithTargetType()
+        {
+            //Arrange
+            var container = new StandardKernel();
+            var containerExtension = new NinjectExtension();
+            containerExtension.InitializeContainer(container);
+
+            //Act
+            containerExtension.RegisterAll<IBootstrapperContainerExtension>();
+            var result = container.GetAll<IBootstrapperContainerExtension>();
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IEnumerable<IBootstrapperContainerExtension>));
+            Assert.IsTrue(result.Count() > 0);
+            Assert.IsTrue(result.Any(c => c is NinjectExtension));
+        }
+
+        [TestMethod]
+        public void ShouldReturnABootstrapperContainerExtensionOptions()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            var result = containerExtension.Options;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IBootstrapperContainerExtensionOptions));
+            Assert.IsInstanceOfType(result, typeof(BootstrapperContainerExtensionOptions));
+        }
+
+        [TestMethod]
+        public void ShouldRegisterWithConventionAndWithRegistration()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+            containerExtension.Options.UsingAutoRegistration();
+
+            //Act
+            containerExtension.Run();
+
+            //Assert
+            Assert.IsNotNull(containerExtension.Resolve<NinjectExtension>());
+            Assert.IsNotNull(containerExtension.Resolve<IRegisteredByConvention>());
+        }
+
+        [TestMethod]
+        public void ShouldThrowNoContainerExceptionWhenSettingServiceLocatorBeforeInitializingTheContainer()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            var result = ExceptionAssert.Throws<NoContainerException>(containerExtension.SetServiceLocator);
+
+            //Assert
+            Assert.AreEqual(NoContainerException.DefaultMessage, result.Message);
+        }
+
+        [TestMethod]
+        public void ShouldThrowNoContainerExceptionWhenResolvingSimpleTypeBeforeInitializingTheContainer()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            var result = ExceptionAssert.Throws<NoContainerException>(() => containerExtension.Resolve<object>());
+
+            //Assert
+            Assert.AreEqual(NoContainerException.DefaultMessage, result.Message);
+        }
+
+        [TestMethod]
+        public void ShouldThrowNoContainerExceptionWhenResolvingMultipleTypesBeforeInitializingTheContainer()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            var result = ExceptionAssert.Throws<NoContainerException>(() => containerExtension.ResolveAll<object>());
+
+            //Assert
+            Assert.AreEqual(NoContainerException.DefaultMessage, result.Message);
+        }
+
+        [TestMethod]
+        public void ShouldThrowNoContainerExceptionWhenRegisteringWithTargetAndImplementationTypeBeforeInitializingTheContainer()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            var result = ExceptionAssert.Throws<NoContainerException>(containerExtension.Register<IBootstrapperContainerExtension, NinjectExtension>);
+
+            //Assert
+            Assert.AreEqual(NoContainerException.DefaultMessage, result.Message);
+        }
+
+        [TestMethod]
+        public void ShouldThrowNoContainerExceptionWhenRegisteringWithTargetAndImplementationInstanceBeforeInitializingTheContainer()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            var result = ExceptionAssert.Throws<NoContainerException>(() => containerExtension.Register<IBootstrapperContainerExtension>(containerExtension));
+
+            //Assert
+            Assert.AreEqual(NoContainerException.DefaultMessage, result.Message);
+        }
+
+        [TestMethod]
+        public void ShouldThrowNoContainerExceptionWhenRegisteringWithTargetTypeBeforeInitializingTheContainer()
+        {
+            //Arrange
+            var containerExtension = new NinjectExtension();
+
+            //Act
+            var result = ExceptionAssert.Throws<NoContainerException>(containerExtension.RegisterAll<IBootstrapperContainerExtension>);
+
+            //Assert
+            Assert.AreEqual(NoContainerException.DefaultMessage, result.Message);
+        }
+    }
+}
