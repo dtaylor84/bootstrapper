4e77d3426e26300c0eea3d8e167dfe99f4d4f07a
Luis Bocaletti <luis.bocaletti@gmail.com>
2012-11-23 22:04:10 -0600

    Started SimpleInjector Branch

---
+++ Tests/Bootstrapper.Tests/Extensions/Containers/SimpleInjector/SimpleInjectorExtensionTests.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bootstrap.Tests.Extensions.Containers.SimpleInjector
+{
+    [TestClass]
+    public class SimpleInjectorExtensionTests
+    {
+    }
+}
